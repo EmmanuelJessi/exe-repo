356d32a331de43cc3083c0219606c704635eb9e2
EmmanuelJessi <107475789+EmmanuelJessi@users.noreply.github.com>
2022-09-21 12:12:58 +0300

    trial

---
+++ home/test.java
@@ -0,0 +1,17 @@
+package home;
+
+public class test {
+
+
+    public static void main(String[] args) {
+
+        int Wage = 250;
+        double Wage_conversion = (double)Wage;
+
+
+        byte W = 20;
+        int C = (int) W;
+
+
+    }
+}
