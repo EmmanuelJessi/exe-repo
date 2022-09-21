94d405d606402b63c08cfb3985f032c36694ce36
EmmanuelJessi <107475789+EmmanuelJessi@users.noreply.github.com>
2022-09-21 15:37:06 +0300

    Create java

--- home/test.java
+++ home/test.java
@@ -5,12 +5,28 @@ public class test {
 
     public static void main(String[] args) {
 
+        char princess = 1;
+        float prince = (float)princess;
+
+
         int Wage = 250;
         double Wage_conversion = (double)Wage;
 
 
         byte W = 20;
         int C = (int) W;
+
+        long E = 170;
+        double f = (double) E;
+
+
+        Short Sabi = 95;
+        int ema = (int) Sabi;
+
+
+        int J = 200;
+        String T = String.valueOf(J);
+
 
 
     }
